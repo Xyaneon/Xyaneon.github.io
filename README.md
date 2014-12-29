5e6bfa5a2d0cb277641f9404fbd7b405df74022f
Christopher Kyle Horton <christhehorton@gmail.com>
2014-12-28 22:32:48 -0500

    First commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+WarriorIng64.github.io
+======================
+
+My personal website. It currently includes:
+
+- Links to various things, including some of my online profiles
+- A list of my personal projects
+- Some basic information about me
+- Instructions to potential employers about how to request a copy of my résumé
+
+It can be accessed at [WarriorIng64.github.io][1] in any modern HTML5 browser. Internet Explorer versions previous to 11 are not supported.
+
+[1]: http://warrioring64.github.io
---
+++ js/index.js
@@ -0,0 +1,103 @@
+function changeSiteLocation(new_text, s_speed) {
+	$("#sitelocation").animate({width: 'toggle'}, {duration: s_speed});
+	setTimeout(function(){
+		$("#sitelocation").text(new_text);
+	}, s_speed);
+	$("#sitelocation").delay(s_speed).animate({width: 'toggle'}, {duration: s_speed});
+}
+
+$(document).ready(function(){
+	var slidespeed = 200;
+	var loadinspeed = 400;
+	$("#navbar-right").animate({"top" : "0px"}, loadinspeed);
+	$("#navbar").delay(loadinspeed / 2.0).animate({"top" : "0px"}, loadinspeed);
+	$("#attribution").slideDown(loadinspeed);
+	$("#homescreen").delay(loadinspeed).fadeIn(loadinspeed)
+	
+	$("#blogbutton").hover(
+		function() {
+			$("#hlrpsb").slideDown(slidespeed);
+		},
+		function () {}
+	);
+	$("#youtubebutton").hover(
+		function() {
+			$("#hlrpsb").slideUp(slidespeed);
+		},
+		function () {}
+	);
+	$("#googleplusbutton").hover(
+		function() {
+			$("#hlrpsb").slideUp(slidespeed);
+		},
+		function () {}
+	);
+	$("#githubbutton").hover(
+		function() {
+			$("#hlrpsb").slideUp(slidespeed);
+		},
+		function () {}
+	);
+	$("#hlrpsb").mouseleave(
+		function() {
+			$("#hlrpsb").slideUp(slidespeed);
+		}
+	);
+	$("#backbutton").click(
+		function() {
+			$(".projectbox").slideUp(slidespeed);
+			$("#projectsheader").fadeOut(slidespeed);
+			$("#resumebody").slideUp(slidespeed);
+			$("#aboutme").slideUp(slidespeed);
+			$("#backbutton").slideUp(slidespeed);
+			$("#mainbox").fadeOut(slidespeed);
+			$("#homescreen").delay(slidespeed).fadeIn(slidespeed);
+			changeSiteLocation(" Home", slidespeed);
+		}
+	);
+	$("#aboutmebutton").click(
+		function() {
+			$("#homescreen").fadeOut(slidespeed);
+			$("#mainstart").fadeOut(slidespeed);
+			$(".projectbox").slideUp(slidespeed);
+			$("#resumebody").slideUp(slidespeed);
+			$("#backbutton").delay(slidespeed).fadeIn(slidespeed);
+			$("#mainbox").delay(slidespeed).fadeIn(slidespeed);
+			$("#aboutme").delay(slidespeed * 2).slideDown(slidespeed);
+			changeSiteLocation(" About Me", slidespeed);
+		}
+	);
+	$("#resumebutton").click(
+		function() {
+			$("#homescreen").fadeOut(slidespeed);
+			$("#mainstart").fadeOut(slidespeed);
+			$("#aboutme").slideUp(slidespeed);
+			$(".projectbox").slideUp(slidespeed);
+			$("#backbutton").delay(slidespeed).fadeIn(slidespeed);
+			$("#mainbox").delay(slidespeed).fadeIn(slidespeed);
+			$("#resumebody").delay(slidespeed * 2).slideDown(slidespeed);
+			changeSiteLocation(" Résumé", slidespeed);
+		}
+	);
+	$("#personalprojectsbutton").click(
+		function() {
+			$("#homescreen").fadeOut(slidespeed);
+			$("#mainstart").fadeOut(slidespeed);
+			$("#aboutme").slideUp(slidespeed);
+			$("#resumebody").slideUp(slidespeed);
+			$("#backbutton").delay(slidespeed).fadeIn(slidespeed);
+			$("#mainbox").delay(slidespeed).fadeIn(slidespeed);
+			$(".projectbox").delay(slidespeed * 2).slideDown(slidespeed);
+			$("#projectsheader").delay(slidespeed * 2).slideDown(slidespeed);
+			changeSiteLocation(" Personal Projects", slidespeed);
+		}
+	);
+	$(".projectbox").click(
+		function() {
+			$(".projectbox").not(this).each(function() {
+				$(this).children(".projectboxcontents").slideUp(slidespeed);
+			});
+			$(this).children(".projectboxcontents").slideToggle(slidespeed);
+		}
+	);
+});
